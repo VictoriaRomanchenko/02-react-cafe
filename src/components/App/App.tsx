bf49a4a3fbd2ea843d4041943d58fb9fce084854
Victoria Romanchenko <romanchenko.victoria@lll.kpi.ua>
2025-07-26 19:51:54 +0300

    updet react-project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import CafeInfo from "../CafeInfo/CafeInfo"
 import type { VoteType, Votes } from "../../types/votes";
-import VoteOptions from "../VoteOption/VoteOptions";
+import VoteOptions from "../VoteOptions/VoteOptions";
 import VoteStats from "../VoteStats/VoteStats";
 import Notification from "../Notification/Notification";
 import css from './App.module.css'
